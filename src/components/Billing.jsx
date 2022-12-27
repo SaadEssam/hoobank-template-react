75f876fe487b4edfa91ea398f032db99946b3466
SaadEssam <SaadEssam059@gmail.com>
2022-12-27 16:28:17 +0200

    CardDeal component

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -11,14 +11,14 @@ const Billing = () => (
       <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
     </div>
 
-    <div className={layout.sectionInfo}>
-      <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" />
+    <div className={`${layout.sectionInfo} items-center`} >
+      <h2 className={`${styles.heading2} max-w-[470px]`}>Easily control your <br className="sm:block hidden" />
       Billing & invoicing.</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>The best way to simplify your invoicing 
       and billing procedure is to choose an easy-to-use tool that offers scalability. The best invoice 
       app will help you implement the same process regardless of the number of clients.</p>
 
-      <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
+      <div className="flex flex-row flex-wrap sm:mt-15 mt-6">
         <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
         <img src={google} alt="google_play" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
         <img src={huawei} alt="huawei_gallery" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[550px] mt-5`}>- Check your credit. <br /> - Identify which type of credit card you need. <br /> - Narrow your choices by asking the right questions. <br /> - Apply for the card that offers you the highest overall value.</p>
+      <Button props="mt-5" />
+    </div>
+    
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+)
 
 export default CardDeal
