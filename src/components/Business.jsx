8cbf15d76f4ce981707738189b519851d758a73a
SaadEssam <SaadEssam059@gmail.com>
2022-12-27 14:32:21 +0200

    Business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,6 +3,22 @@ import { features } from '../constants'
 import styles, { layout } from '../style'
 import Button from './Button'
 
+const FeatureCard = ({ icon, title, content, index}) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue mr-3`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div classNam="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+)
+
 const Business = () => (
   <section id="features" className={layout.section}>
     <div className={layout.sectionInfo}>
@@ -12,6 +28,12 @@ const Business = () => (
       you can improve your financial life by building credit, earning rewards and saving money. 
       But with hundreds of credit cards on the market.</p>
       <Button props="mt-10" />
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
     </div>
   </section>
 )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({props}) => {
   return (
     <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium
-    text-[18px] text-primary outline-none ${props}`}>
+    text-[18px] text-primary outline-none rounded-[10px] ${props}`}>
       Get Started
     </button>
   )
